bbd7a7955c9b4757fead71b88cd60e93f9e77424
rymph <rymph0501@gmail.com>
2025-08-18 16:00:38 +0900

    bootJar disabled for spring library conventions

--- src/main/kotlin/com.quadra.spring-library-conventions.gradle.kts
+++ src/main/kotlin/com.quadra.spring-library-conventions.gradle.kts
@@ -1,3 +1,5 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     id("com.quadra.library-conventions")
     id("org.springframework.boot")
@@ -18,4 +20,12 @@ configurations.all {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.named<BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.named<Jar>("jar") {
+    enabled = true
 }
