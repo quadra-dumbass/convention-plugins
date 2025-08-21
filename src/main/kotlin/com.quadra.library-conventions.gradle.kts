8efb69510f5a4844632e768eb153e6600819bd6c
rymph <rymph0501@gmail.com>
2025-08-21 22:45:26 +0900

    dd

--- src/main/kotlin/com.quadra.library-conventions.gradle.kts
+++ src/main/kotlin/com.quadra.library-conventions.gradle.kts
@@ -6,24 +6,26 @@ plugins {
 
 group = "com.quadra.common"
 
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/quadra-dumbass/common-module")
-            credentials {
-                username = project.findProperty("gpr.module.user") as String? ?: System.getenv("GITHUB_MODULE_USER")
-                password = project.findProperty("gpr.module.token") as String? ?: System.getenv("GITHUB_MODULE_TOKEN")
+project.afterEvaluate {
+    publishing {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/quadra-dumbass/common-module")
+                credentials {
+                    username = project.findProperty("gpr.module.user") as String? ?: System.getenv("GITHUB_MODULE_USER")
+                    password = project.findProperty("gpr.module.token") as String? ?: System.getenv("GITHUB_MODULE_TOKEN")
+                }
             }
         }
-    }
 
-    publications {
-        create<MavenPublication>("gpr") {
-            from(components["java"])
-            groupId = project.findProperty("groupId") as String? ?: project.group as String
-            artifactId = project.findProperty("artifactId") as String? ?: project.name
-            version = project.findProperty("version") as String? ?: project.version as String
+        publications {
+            create<MavenPublication>("gpr") {
+                from(components["java"])
+                groupId = project.findProperty("groupId") as String? ?: project.group as String
+                artifactId = project.findProperty("artifactId") as String? ?: project.name
+                version = project.findProperty("version") as String? ?: project.version as String
+            }
         }
     }
 }
